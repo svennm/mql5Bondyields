f0452be3cc2743a87724b1f00232f76b22352f92
svennm <svennmivedor@gmail.com>
2022-02-26 21:39:33 -0600

    Update bondyields_webscraped.py

--- bondyields_webscraped.py
+++ bondyields_webscraped.py
@@ -1,12 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-
-
-
-
-
-
 TenYearTreasury = "https://www.marketwatch.com/investing/bond/tmubmusd10y?countrycode=bx"
 TwoYearTreasury = "https://www.marketwatch.com/investing/bond/tmubmusd02y?countrycode=bx"
 FiveYearTreasury = "https://www.marketwatch.com/investing/bond/tmubmusd05y?countrycode=bx"
@@ -18,9 +12,17 @@ page5y = requests.get(FiveYearTreasury)
 soup10Y = BeautifulSoup(page10y.content, 'html.parser')
 TenYearYield = soup10Y.find_all("span", class_ = "value")
 
+#this loop prints out the text of all the span with the class "value +" in the list printed out by this the value that we want is the last one
+
+TenYearYields = []
 for i in TenYearYield:
     print(i.text,end="\n"*2)
     
+      
+    
+
+
+
 
 
 # print(TenYearYield.text)
