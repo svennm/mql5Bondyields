f2aff2ec628e1ae9446de8f4c116ba848e3f36c4
svennm <svennmivedor@gmail.com>
2022-03-10 14:53:09 -0600

    Update bondyields_webscraped.py

--- bondyields_webscraped.py
+++ bondyields_webscraped.py
@@ -13,21 +13,20 @@ page5y = requests.get(FiveYearTreasury)
 
 
 soup10Y = BeautifulSoup(page10y.content, 'html.parser')
-TenYearYield = soup10Y.find_all("span", class_ = "value")
+TenYearYield = soup10Y.find_all("bg-quote", class_ = "value")
 
 #this loop prints out the text of all the span with the class "value +" in the list printed out by this the value that we want is the last one
+def US10Y():
+        for i in TenYearYield:
+            Yield10Array=[] 
+            Yield10Array.append(i.text)
+            #print(i.text,end="\n"*2)
+            return(Yield10Array[0])
+            
 
-
-YieldArray = np.zeros((8))
-print(YieldArray) 
-for i in TenYearYield:
-    YieldArray[i] = i
-    print(YieldArray)
-    
-    
+print(US10Y())
       
     
-
 
 
 
